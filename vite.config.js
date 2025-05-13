98dd161d4ac68de1a2cfd3a9f49ea7151a0c02d0
M KARTIKEYA DATTA <kartikeyadatta3@gmail.com>
2025-05-13 10:08:37 -0500

    updated the port details for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // ensures external/Docker access
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost', // or 'host.docker.internal' if inside Docker
+      port: 3000,
+    },
+    overlay: false,
+  },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.[jt]sx?$/,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
+  },
+});
